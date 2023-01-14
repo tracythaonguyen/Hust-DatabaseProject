1f763bfb03dba12595740647e99a42c73b5fd297
ktrung1709 <ktrung1709@gmail.com>
2023-01-14 21:49:27 +0700

    trung fix ngay 14/1

--- create_db.sql
+++ create_db.sql
@@ -1,12 +1,14 @@
--- 
--- Database Script Group 7
--- 
--- 
+create database thegioididong;
+
+create schema sys;
+create schema sales;
+create schema product;
+
 -- Name: roles; Type: TABLE
 -- 
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS sys.roles;
 
-CREATE TABLE roles (
+CREATE TABLE sys.roles (
     role_id INT,
     role_name VARCHAR(255),
     PRIMARY KEY (role_id)
@@ -16,9 +18,9 @@ CREATE TABLE roles (
 -- Name: account; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS sys.accounts;
 
-CREATE TABLE account (
+CREATE TABLE sys.accounts (
   account_id BIGSERIAL,
 	user_name VARCHAR(255) unique,
 	password VARCHAR(255),
@@ -26,16 +28,16 @@ CREATE TABLE account (
   PRIMARY KEY (account_id),
   CONSTRAINT fk_role
     FOREIGN KEY (role_id) 
-      REFERENCES roles(role_id)
+      REFERENCES sys.roles(role_id)
 );
 
 -- 
 -- Name: customers; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS sales.customers;
 
-CREATE TABLE customers (
+CREATE TABLE sales.customers (
   customer_id BIGSERIAL,
 	first_name VARCHAR(255),
 	last_name VARCHAR(255),
@@ -43,70 +45,64 @@ CREATE TABLE customers (
 	email VARCHAR(255) unique,
 	street VARCHAR(255),
 	city VARCHAR(255),
-	zip_code VARCHAR(255),
 	account_id BIGINT unique,
   PRIMARY KEY (customer_id),
   CONSTRAINT fk_account
     FOREIGN KEY (account_id) 
-      REFERENCES account(account_id)
+      REFERENCES sys.accounts(account_id)
 );
 
 -- 
 -- Name: staffs; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS staffs;
+DROP TABLE IF EXISTS sales.staffs;
 
-CREATE TABLE staffs (
-  staff_id BIGSERIAL,
+CREATE TABLE sales.staffs (
+  	staff_id BIGSERIAL,
 	first_name VARCHAR(255),
 	last_name VARCHAR(255),
 	phone VARCHAR(255) unique,
 	email VARCHAR(255) unique,
 	street VARCHAR(255),
 	city VARCHAR(255),
-	zip_code VARCHAR(255),
-	account_id BIGINT unique,
 	active boolean,
   manager_id BIGINT,
   PRIMARY KEY (staff_id),
-  CONSTRAINT fk_account
-    FOREIGN KEY (account_id) 
-      REFERENCES account(account_id)
 );
 
-alter table staffs 
+alter table sales.staffs 
 add constraint fk_staff_id 
-foreign key (manager_id) references staffs(staff_id);
+foreign key (manager_id) references sales.staffs(staff_id);
 -- 
 -- Name: orders; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS sales.orders;
 
-CREATE TABLE orders (
+CREATE TABLE sales.orders (
   order_id BIGSERIAL,
 	customer_id BIGINT,
 	order_status INT,
 	order_date DATE,
 	staff_id BIGINT,
+	total_amount decimal(10,2),
   CHECK(order_status IN (0, 1, 2)),
   PRIMARY KEY (order_id),
   CONSTRAINT fk_customer
     FOREIGN KEY (customer_id)
-      REFERENCES customers(customer_id),
+      REFERENCES sales.customers(customer_id),
   CONSTRAINT fk_staff
     FOREIGN KEY (staff_id)
-      REFERENCES staffs(staff_id) 
+      REFERENCES sales.staffs(staff_id) 
 );
-
 -- 
 -- Name: brands; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS brands;
+DROP TABLE IF EXISTS product.brands;
 
-CREATE TABLE brands (
+CREATE TABLE product.brands (
   brand_id BIGSERIAL,
   brand_name VARCHAR(255),
   PRIMARY KEY (brand_id)
@@ -116,9 +112,9 @@ CREATE TABLE brands (
 -- Name: categories; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS product.categories;
 
-CREATE TABLE categories (
+CREATE TABLE product.categories (
   category_id BIGSERIAL,
   category_name VARCHAR(255),
   PRIMARY KEY (category_id)
@@ -128,46 +124,48 @@ CREATE TABLE categories (
 -- Name: products; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product.products;
 
-CREATE TABLE products (
+CREATE TABLE product.products (
   product_id BIGSERIAL,
 	product_name VARCHAR(255),
 	brand_id BIGINT,
 	category_id BIGINT,
 	model_year CHAR(4),
 	list_price DECIMAL(10,2),
+	avg_rating decimal(1,1) default 0,
+	total_review bigint default 0,
   PRIMARY KEY (product_id),
   CONSTRAINT fk_brand
     FOREIGN KEY (brand_id)
-      REFERENCES brands(brand_id),
+      REFERENCES product.brands(brand_id),
   CONSTRAINT fk_category
     FOREIGN KEY (category_id)
-      REFERENCES categories(category_id)
+      REFERENCES product.categories(category_id)
 );
 
 -- 
 -- Name: stocks; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS product.stocks;
 
-CREATE TABLE stocks (
+CREATE TABLE product.stocks (
   product_id BIGINT,
-	quantity BIGINT,
+	quantity BIGINT check (quantity >= 0),
   PRIMARY KEY (product_id),
   CONSTRAINT fk_stock
     FOREIGN KEY (product_id)
-      REFERENCES products(product_id)
+      REFERENCES product.products(product_id)
 );
 
 -- 
 -- Name: items; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS product.items;
 
-CREATE TABLE items (
+CREATE TABLE product.items (
 	serial_code VARCHAR(255),
 	product_id BIGINT,
 	MFG DATE,
@@ -177,42 +175,42 @@ CREATE TABLE items (
   PRIMARY KEY (serial_code),
   CONSTRAINT fk_product
     FOREIGN KEY (product_id)
-      REFERENCES products(product_id)
+      REFERENCES product.products(product_id)
 );
 
 -- 
 -- Name: order_details; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS order_details;
+DROP TABLE IF EXISTS sales.order_details;
 
-CREATE TABLE order_details (
+CREATE TABLE sales.order_details (
   order_detail_id BIGSERIAL,
 	order_id BIGINT,
-	serial_code VARCHAR(255),
+	serial_code VARCHAR(255) unique,
 	discount DECIMAL(4,2), check (discount between 0 and 1),
   PRIMARY KEY (order_detail_id),
   CONSTRAINT fk_serial_code
     FOREIGN KEY (serial_code)
-      REFERENCES items(serial_code),
+      REFERENCES product.items(serial_code),
   CONSTRAINT fk_order
     FOREIGN KEY (order_id)
-      REFERENCES orders(order_id)
+      REFERENCES sales.orders(order_id)
 );
 
 -- 
 -- Name: coverages; Type: TABLE
 -- 
 
-DROP TABLE IF EXISTS coverages;
+DROP TABLE IF EXISTS sales.coverages;
 
-CREATE TABLE coverages (
+CREATE TABLE sales.coverages (
   coverage_id BIGSERIAL,
-  serial_code INT,
+  serial_code varchar(255) unique,
   coverages_expired_date date,
   PRIMARY KEY (coverage_id),
   CONSTRAINT fk_coverage
   FOREIGN KEY (serial_code)
-  REFERENCES order_details(serial_code)
+  REFERENCES sales.order_details(serial_code)
 );
 
---
+++ insert_data.sql
@@ -0,0 +1,42 @@
+-- Roles
+delete from sys.roles
+
+insert into sys.roles values (0, 'Customer');
+insert into sys.roles values (1, 'Staff');
+insert into sys.roles values (2, 'Product Manager');
+insert into sys.roles values (3, 'Sales Manager');
+
+select * from sys.roles;
+
+-- Account
+drop procedure sys.new_account
+
+create or replace procedure sys.new_account(user_name1 varchar(255), pass_word1 varchar (255), role_id1 bigint)
+language plpgsql
+as $$
+begin
+	execute 'create user ' || user_name1 || ' with PASSWORD ''' || pass_word1 || ''';';
+	insert into sys.accounts (user_name, password, role_id) values (user_name1, pass_word1, role_id1);
+    if role_id1 = 2 THEN
+        execute 'GRANT SELECT, UPDATE, INSERT ON products TO '|| user_name1 ||';';
+		execute 'GRANT SELECT, UPDATE, INSERT ON items TO '|| user_name1 || ';';
+	elseif role_id1 = 3 then
+		execute 'GRANT SELECT, UPDATE, INSERT ON orders TO '|| user_name1 || ';';
+		execute 'GRANT SELECT, UPDATE, INSERT ON order_details TO '|| user_name1 || ';';
+		execute 'GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO '|| user_name1 || ';';
+    END if;
+end $$;
+
+call new_account('ktrung1709', '17092002', 2);
+call new_account('thaobaymau', 'thao123', 3);
+call new_account('dtm', 'minhdo123', 0);
+
+DROP OWNED BY ktrung1709;
+drop owned by thaobaymau;
+drop user ktrung1709;
+drop user thaobaymau;
+
+select * from accounts
+delete from accounts
+
+--order
