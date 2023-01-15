fb74d731e9a057d7159e483bf6ad5e81cdf48f4d
ktrung1709 <ktrung1709@gmail.com>
2023-01-15 16:34:39 +0700

    trung check xong data product, merge 2 file product va item thanh 1

--- insert_data_sys.sql
+++ insert_data_sys.sql
@@ -2,9 +2,8 @@
 delete from sys.roles
 
 insert into sys.roles values (0, 'Customer');
-insert into sys.roles values (1, 'Staff');
-insert into sys.roles values (2, 'Product Manager');
-insert into sys.roles values (3, 'Sales Manager');
+insert into sys.roles values (1, 'Product Manager');
+insert into sys.roles values (2, 'Sales Manager');
 
 select * from sys.roles;
 
@@ -17,18 +16,20 @@ as $$
 begin
 	execute 'create user ' || user_name1 || ' with PASSWORD ''' || pass_word1 || ''';';
 	insert into sys.accounts (user_name, password, role_id) values (user_name1, pass_word1, role_id1);
-    if role_id1 = 2 THEN
-        execute 'GRANT SELECT, UPDATE, INSERT ON products TO '|| user_name1 ||';';
-		execute 'GRANT SELECT, UPDATE, INSERT ON items TO '|| user_name1 || ';';
-	elseif role_id1 = 3 then
-		execute 'GRANT SELECT, UPDATE, INSERT ON orders TO '|| user_name1 || ';';
-		execute 'GRANT SELECT, UPDATE, INSERT ON order_details TO '|| user_name1 || ';';
-		execute 'GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO '|| user_name1 || ';';
-    END if;
+-- Đoạn này chưa viết xong procedure nên chưa phân quyền tạm để trống
+
+--     if role_id1 = 1 THEN
+--         execute 'GRANT SELECT, UPDATE, INSERT ON products TO '|| user_name1 ||';';
+-- 		execute 'GRANT SELECT, UPDATE, INSERT ON items TO '|| user_name1 || ';';
+-- 	elseif role_id1 = 2 then
+-- 		execute 'GRANT SELECT, UPDATE, INSERT ON orders TO '|| user_name1 || ';';
+-- 		execute 'GRANT SELECT, UPDATE, INSERT ON order_details TO '|| user_name1 || ';';
+-- 		execute 'GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO '|| user_name1 || ';';
+--     END if;
 end $$;
 
 call new_account('ktrung1709', '17092002', 2);
-call new_account('thaobaymau', 'thao123', 3);
+call new_account('thaobaymau', 'thao123', 1);
 call new_account('dtm', 'minhdo123', 0);
 
 DROP OWNED BY ktrung1709;
