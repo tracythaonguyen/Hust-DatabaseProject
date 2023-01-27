b089dafe53bdbed2da8d8201e7302b621af12740
dotuanminh <78138604+dotuanminh@users.noreply.github.com>
2023-01-28 02:42:07 +0700

    commit backend part 2

--- thegioididong/src/main/java/hust/globalict/entity/product/Category.java
+++ thegioididong/src/main/java/hust/globalict/entity/product/Category.java
@@ -1,5 +1,29 @@
 package hust.globalict.entity.product;
 
-public class Category {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "product.categories", catalog = "thegioididong")
+public class Category implements Serializable{
+	@Column(name = "category_id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long category_id;
+	
+	@Column(name = "category_name", length = 255, nullable = false, unique = true)
+	private String category_name;
 }
