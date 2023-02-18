4fe7ee6ecf088aac7c0cf936c2b9889322ec42f1
dotuanminh <78138604+dotuanminh@users.noreply.github.com>
2023-02-19 01:47:01 +0700

    commit

--- thegioididong/src/main/java/hust/globalict/controller/CategoryController.java
+++ thegioididong/src/main/java/hust/globalict/controller/CategoryController.java
@@ -2,11 +2,24 @@ package hust.globalict.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import hust.globalict.repository.BrandRepository;
 import hust.globalict.repository.CategoryRepository;
 
 @Controller
 public class CategoryController {
 	@Autowired
-    private CategoryRepository categoryRepo;
+	private CategoryRepository categoryRepo;
+
+	public CategoryController(CategoryRepository categoryRepo) {
+		this.categoryRepo = categoryRepo;
+	}
+
+	@GetMapping("/categories")
+	public String listCategoryrand(Model model) {
+		model.addAttribute("categories", categoryRepo.listAll());
+		return "categories";
+	}
 }
--- thegioididong/src/main/java/hust/globalict/repository/CategoryRepository.java
+++ thegioididong/src/main/java/hust/globalict/repository/CategoryRepository.java
@@ -1,11 +1,20 @@
 package hust.globalict.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import hust.globalict.entity.products.Brand;
 import hust.globalict.entity.products.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
-
+	@Query(value = "SELECT * FROM product.categories",nativeQuery = true)
+	List<Category> listAll();
+	
+	@Query(value = "CALL product.new_category(:category_name)",nativeQuery = true)
+	void addNewCategory(@Param("category_name")String category_name);
 }
