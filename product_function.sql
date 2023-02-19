8b68f7bdc4403e6738c57c568d4d0be32637f2c0
Thao Nguyen <thaophuong.tracy@gmail.com>
2023-02-19 17:20:24 +0700

    add: update & add product

--- product_function.sql
+++ product_function.sql
@@ -123,26 +123,82 @@ BEGIN
 	SET discontinued=TRUE
 	WHERE product_id = id;
 END;
-
-
 $$;
 
+CREATE OR REPLACE FUNCTION search_id_brand(brand_name VARCHAR) RETURNS BIGINT
+LANGUAGE plpgsql
+AS $$
+DECLARE brand_id BIGINT;
+BEGIN
+	SELECT b.brand_id INTO brand_id FROM product.brands b
+	WHERE b.brand_name = $1;
+	RETURN brand_id;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION search_id_category(category_name VARCHAR) RETURNS BIGINT
+LANGUAGE plpgsql
+AS $$
+DECLARE category_id BIGINT;
+BEGIN
+	SELECT c.category_id INTO category_id FROM product.categories c
+	WHERE c.category_name = $1;
+	RETURN category_id;
+END;
+$$;
+
+-- update product
 DROP PROCEDURE IF EXISTS product.update_product;
-CREATE PROCEDURE product.update_product(product_id BIGINT, product_name VARCHAR(255), brand_name VARCHAR(255), category_name VARCHAR(255), model_year CHAR(4), list_price DECIMAL(10,2), avg_rating DECIMAL(1,1), total_review BIGINT)
+CREATE PROCEDURE product.update_product(product_id BIGINT, product_name VARCHAR(255), brand_name VARCHAR(255), category_name VARCHAR(255), model_year CHAR(4), list_price DECIMAL(10,2))
 LANGUAGE plpgsql
 AS $$
+DECLARE brand_ids BIGINT;
+				category_ids BIGINT;
 BEGIN
---
+	SELECT search_id_brand($3) INTO brand_ids;
+	SELECT search_id_category($4) INTO category_ids;
+
+	IF brand_ids IS NULL
+	THEN
+		CALL product.new_brand(brand_name);
+		SELECT search_id_brand($3) INTO brand_ids;
+	END IF;
+
+	IF category_ids IS NULL
+	THEN 
+		CALL product.new_category(category_name);
+		SELECT search_id_category($4) INTO category_ids;
+	END IF;
+
+	UPDATE product.products p
+	SET product_name = $2, brand_id = brand_ids, category_id = category_ids, model_year = $5, list_price = $6
+	WHERE p.product_id = $1;
 END;
 $$;
 
+-- add product
 DROP PROCEDURE IF EXISTS product.add_product;
-CREATE PROCEDURE product.add_product(product_id BIGINT, product_name VARCHAR(255), brand_name VARCHAR(255), category_name VARCHAR(255), model_year CHAR(4), list_price DECIMAL(10,2), avg_rating DECIMAL(1,1), total_review BIGINT)
+CREATE PROCEDURE product.add_product(product_name VARCHAR(255), brand_name VARCHAR(255), category_name VARCHAR(255), model_year CHAR(4), list_price DECIMAL(10,2))
 LANGUAGE plpgsql
 AS $$
+DECLARE brand_id BIGINT;
+				category_id BIGINT;
 BEGIN
---
+	SELECT search_id_brand($2) INTO brand_id;
+	SELECT search_id_category($3) INTO category_id;
+
+	IF brand_id IS NULL
+	THEN
+		CALL product.new_brand(brand_name);
+		SELECT search_id_brand($2) INTO brand_id;
+	END IF;
+
+	IF category_id IS NULL
+	THEN 
+		CALL product.new_category(category_name);
+		SELECT search_id_category($3) INTO category_id;
+	END IF;
+
+	INSERT INTO product.products(product_name, brand_id, category_id, model_year, list_price) VALUES (product_name, brand_id, category_id, model_year, list_price);
 END;
 $$;
-
--- còn nhiều, chưa viết hết
