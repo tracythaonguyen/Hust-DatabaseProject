eb757e094e794fab525a65ba6b4b86600abd49ad
dotuanminh <78138604+dotuanminh@users.noreply.github.com>
2023-02-19 21:31:46 +0700

    commit

--- thegioididong/src/main/java/hust/globalict/controller/CategoryController.java
+++ thegioididong/src/main/java/hust/globalict/controller/CategoryController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import hust.globalict.repository.BrandRepository;
+import hust.globalict.entity.products.Brand;
+import hust.globalict.entity.products.Category;
 import hust.globalict.repository.CategoryRepository;
 
 @Controller
@@ -19,6 +21,20 @@ public class CategoryController {
 
 	@GetMapping("/categories")
 	public String listCategoryrand(Model model) {
+		model.addAttribute("categories", categoryRepo.listAll());
+		return "categories";
+	}
+	@GetMapping("/categories/new")
+	public String createCategoryForm(Model model) {
+		Category category = new Category();
+		model.addAttribute("category", category);
+		return "create_category";
+	}
+	
+	@GetMapping("/categories/save")
+	public String saveCategory(@ModelAttribute("category") Category category,Model model) {
+		model.addAttribute("category_name", category.getCategory_name());
+		categoryRepo.addNewCategory(category.getCategory_name());
 		model.addAttribute("categories", categoryRepo.listAll());
 		return "categories";
 	}
