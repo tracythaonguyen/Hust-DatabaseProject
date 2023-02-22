06170d6041aca27edab83eaed73707a51c2ec790
Thao Nguyen <thaophuong.tracy@gmail.com>
2023-02-22 09:14:04 +0700

    add: order history GUI

--- thegioididong/src/main/java/hust/globalict/controller/OrderController.java
+++ thegioididong/src/main/java/hust/globalict/controller/OrderController.java
@@ -1,12 +1,46 @@
 package hust.globalict.controller;
 
+import hust.globalict.entity.sales.CustomerInfo;
+import hust.globalict.entity.sales.Order;
+import hust.globalict.entity.sys.Account;
+import hust.globalict.repository.AccountRepository;
+import hust.globalict.repository.CustomerInfoRepository;
+import hust.globalict.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import hust.globalict.repository.OrderRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrderController {
-	@Autowired
-    private OrderRepository orderRepo;
+  @Autowired
+  private OrderRepository orderRepo;
+
+  @Autowired
+  private AccountRepository accountRepo;
+
+  @Autowired
+  private CustomerInfoRepository customerInfoRepo;
+
+  public OrderController(OrderRepository orderRepo) {
+    this.orderRepo = orderRepo;
+  }
+
+  @GetMapping("/order_history/{id:.+}")
+  public String orderHistory(@PathVariable Long id, Model model) {
+    Account account = accountRepo.findAccountById(id + 1);
+    model.addAttribute("account", account);
+
+    CustomerInfo customer = customerInfoRepo.findCustomerByAccountId(
+      account.getAccount_id()
+    );
+    model.addAttribute("customer", customer);
+
+    Order order = orderRepo.findOrderById(customer.getCustomer_id());
+    model.addAttribute("order", order);
+
+    return "order_history";
+  }
 }
--- thegioididong/src/main/java/hust/globalict/entity/sales/Order.java
+++ thegioididong/src/main/java/hust/globalict/entity/sales/Order.java
@@ -2,25 +2,18 @@ package hust.globalict.entity.sales;
 
 import java.io.Serializable;
 import java.sql.Date;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -28,28 +21,24 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @Entity
-@Table(name = "sales.orders", catalog = "thegioididong")
-public class Order implements Serializable{
-	@Column(name = "order_id")
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long order_id;
-	
-	@JoinColumn(name = "customer_id")
-	@ManyToOne(cascade = CascadeType.ALL, optional = false)
-	private Customer customer;
-	
-	@Column(name = "order_status")
-	private int order_status;
-	
-	@Column(name = "order_date")
-	@DateTimeFormat(pattern="yyyy-mm-dd")
-    private Date order_date;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "staff_id", referencedColumnName = "staff_id")
-	private Staff staff;
-	
-	@Column(name = "total_amount")
-	private float total_amount;
+@Table(name = "sales.view_order_history", catalog = "thegioididong")
+public class Order implements Serializable {
+  @Column(name = "order_id")
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long order_id;
+
+  @Column(name = "order_status")
+  private String order_status;
+
+  @Column(name = "order_date")
+  @DateTimeFormat(pattern = "yyyy-mm-dd")
+  private Date order_date;
+
+  @Column(name = "staff_id")
+  // @OneToOne(cascade = CascadeType.ALL)
+  private long staff_id;
+
+  @Column(name = "total_amount")
+  private double total_amount;
 }
